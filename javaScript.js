a6d2843b29c04e7c2d988ed6838077c9a93d20cc
null-heap <142169162+null-heap@users.noreply.github.com>
2024-03-28 13:54:41 +0200

    freat: snake like hovering effect on grid description: created the hovering effect through js

--- javaScript.js
+++ javaScript.js
@@ -1,7 +1,10 @@
 const divArray = [];
-const rows = 16;
-const columns = 17;
+const rows = 68;
+const columns = 68;
 let mainDiv = document.querySelector('#main');
+
+
+
 
 for(let i = 0;i < rows; i++){
     let temp = document.createElement('div');
@@ -15,3 +18,29 @@ for(let i = 0;i < rows; i++){
         divArray[i][0].appendChild(divArray[i][j]);
     }
 }
+
+let count = 0;
+const hoverArray = [];
+
+//hovering effect
+mainDiv.addEventListener('mouseover',(e) =>{
+    let delayInMIlliseconds = 2000;
+     
+    if( e.target !== mainDiv && e.target.className !== "firstRow") {
+    let target = e.target;
+    target.style.cssText = `background-color: yellow`;
+    
+
+    //makes snake like affect 
+    hoverArray.push(target);
+    if(count == 16){
+        let temp = hoverArray.shift();
+        temp.style.cssText = 'background-color: transparent';
+        count--;
+    }
+    count ++;
+
+    }
+
+
+})
